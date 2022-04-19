3a74749aa65577ef13ef1f9dc1a3cd22f469d769
Dilini Jayasiri <dilininadeesha2018@gmail.com>
2022-04-19 09:35:29 +0530

    updated donate dash

--- donatefoodweb/src/components/DonateDash.jsx
+++ donatefoodweb/src/components/DonateDash.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 
 
+
  const DonateDash = () => {
   return (
     <div>
-         <div className="container shadow my-5">
+         <div className="container shadow my-5 form">
                <div className="row">
-                   <div className="col-md-5 d-flex flex-column align-items-center text-white justify-content-center form">
+                   <div className="col-md-5 d-flex flex-column align-items-center justify-content-center dashForm">
                        <h1 className="display-4 fw-bolder">We can accept</h1>
                        <ul>
                            <li> Short coded product</li>
@@ -20,14 +21,14 @@ import { NavLink } from 'react-router-dom';
                            <li>New product lines or promotions which didn’t take off</li>
                            <li>Product withdrawals if they’re still fit for use</li>
                        </ul>
-                      
-                      
+                     
                    </div>
+                   <div className='col-md-5 d-flex'><img src="/assets/accept.gif" className="w-50 mt-5"/></div>
                </div>
            </div>
-           <div className="container shadow my-5">
-               <div className="row justify-content-end">
-                   <div className="col-md-5 d-flex flex-column align-items-center text-white justify-content-center form order-2">
+           <div className="container shadow my-5 form">
+               <div className="row justify-content-end"> 
+                   <div className="col-md-5 d-flex flex-column align-items-center justify-content-center order-2 ">
                        <h1 className="display-4 fw-bolder">We Don't Accept</h1>
                          <ul>
                            <li>Regulated products such as alcohol, tobacco or drugs of dependence</li>
